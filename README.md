86a99444000f460bea857a6c02c1f4d26d20390e
leesh <lsh80165@gmail.com>
2022-03-07 17:15:36 +0900

    mvc_2_section_2

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # spring_boot_mvc_2
+
 인프런 - 스프링 MVC 2편
+
+> 인프런 - 스프링 MVC 2편 강의를 듣고 공부한 코드와 내용정리한 노션주소를 올리는 저장소
+
+### 섹션 1. https://www.notion.so/MVC-2-1-870598ac24344747bcf4262ea5c23ae3
+
+### 섹션 2. https://www.notion.so/MVC-2-2-8320e5091b7345de89303728e13e8529
---
+++ form-start/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -0,0 +1,122 @@
+package hello.itemservice.web.form;
+
+import hello.itemservice.domain.item.DeliveryCode;
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequestMapping("/form/items")
+@RequiredArgsConstructor
+public class FormItemController {
+
+    private final ItemRepository itemRepository;
+
+    //각 매핑마다 넣어줘야되기 때문에 반복이 많이 발생한다. 반복을 줄여주기 위해서 이러한것을 제공한다.
+    //어떤것이 호출되던, 모델에 아래의 로직데이터가 담김
+    //static으로 해두고 어딘가에서 불러오는 식으로 최적화를 생각해보는것이 좋음
+    @ModelAttribute("regions")
+    public Map<String,String> regions(){
+        Map<String,String> regions = new LinkedHashMap<>(); //순서 보장을 위해서 링크드 해시맵 사용
+
+        regions.put("SEOUL","서울");
+        regions.put("BUSAN","부산");
+        regions.put("JEJU","제주");
+        return regions;
+    }
+
+    //컨트롤 알트 n 을 사용하면 인라인 기능으로 코드를 합칠수 있음
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes(){
+        return ItemType.values();
+    }
+
+    //위에서도 언급했지만, 이는 컨트롤러 호출시마다 호출되므로,미리 별도로 생성해두고, 불러 쓰는것이 효율적
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes(){
+        List<DeliveryCode> deliverCodes = new ArrayList<>();
+        deliverCodes.add(new DeliveryCode("FAST","빠른 배송"));
+        deliverCodes.add(new DeliveryCode("NORMAL","일반 배송"));
+        deliverCodes.add(new DeliveryCode("SLOW","느린 배송"));
+
+        return deliverCodes;
+    }
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+//        Map<String,String> regions = new LinkedHashMap<>(); //순서 보장을 위해서 링크드 해시맵 사용
+//
+//        regions.put("SEOUL","서울");
+//        regions.put("BUSAN","부산");
+//        regions.put("JEJU","제주");
+//        model.addAttribute("regions",regions);
+        return "form/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "form/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item",new Item());
+//        Map<String,String> regions = new LinkedHashMap<>(); //순서 보장을 위해서 링크드 해시맵 사용
+//
+//        regions.put("SEOUL","서울");
+//        regions.put("BUSAN","부산");
+//        regions.put("JEJU","제주");
+//        model.addAttribute("regions",regions);
+
+        return "form/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        log.info("item.open={}",item.getOpen());
+        log.info("item.regions={}", item.getRegions());
+        log.info("item.itemType={}", item.getItemType());
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/form/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+//        Map<String,String> regions = new LinkedHashMap<>(); //순서 보장을 위해서 링크드 해시맵 사용
+//
+//        regions.put("SEOUL","서울");
+//        regions.put("BUSAN","부산");
+//        regions.put("JEJU","제주");
+//        model.addAttribute("regions",regions);
+        return "form/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/form/items/{itemId}";
+    }
+
+}
+
